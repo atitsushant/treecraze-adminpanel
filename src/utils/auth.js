864c78b84a8a0dfffcdc26c072f89a2814ce1c9e
Sushant sharma <atitsushant@gmail.com>
2025-09-27 19:20:53 +0530

    Initial commit for treecraze-backend

--- src/utils/auth.js
+++
@@ -1,6 +0,0 @@
-export const login = async (username, password) => {
-  const res = await api.post('/token/', { username, password });
-  localStorage.setItem('token', res.data.access);
-};
-
-export const getToken = () => localStorage.getItem('token');
