a2b4f6533a62f988f317290046b436777ae2f7a2
Sushant sharma <atitsushant@gmail.com>
2025-09-27 19:00:03 +0530

    Initial commit for treecraze-backend

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,8 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
+import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
-  plugins: [react()],
-  css: {
-    postcss: './postcss.config.cjs',
-  },
-});
+  plugins: [
+    tailwindcss(),
+  ],
+})
